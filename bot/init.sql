15a0aa92c6fed8246841a49571cdc9de43702855
Kidus Adugna <kidusadugna@gmail.com>
2019-10-05 08:20:26 +0300

    improve the data model

--- bot/init.sql
+++ bot/init.sql
@@ -1,31 +1,29 @@
-/* DROP TABLE posts; */
-/* DROP TABLE sessions; */
+/* DATA MODEL FOR THE BOT */
 
 /* admins of channels */
-CREATE TABLE admins (username VARCHAR(255) PRIMARY KEY);
+CREATE TABLE admins (username VARCHAR(255) PRIMARY KEY,
+                     chat_id VARCHAR(255),
+                     draft_title VARCHAR(255),
+                     draft_description VARCHAR(3000),
+                     /* json encoded list of image ids, the first is the collage */
+                     draft_image_ids VARCHAR(3000),
+                     draft_stage VARCHAR(255), /* where the admin is in the publishing of the post */
+                     caption_template VARCHAR(1024) DEFAULT ':title\n\n:description'
+);
 
 /* channels owned by admins, an admin can have more than one channel */
 CREATE TABLE channels (username VARCHAR(255) PRIMARY KEY,
                        admin VARCHAR(255),
+                       contact_text VARCHAR(255),
                        FOREIGN KEY (admin) REFERENCES admins(username)
 );
 
-/* posts are assumed to be by the bot,
- message_id is in the form '@channel/message_id' because message_id's are only unique inside chats */
+/* posts by the bot,
+ message_id is in the form 'channel/message_id' because message_id's are only unique inside chats */
 CREATE TABLE posts (message_id VARCHAR(255) PRIMARY KEY,
                     channel VARCHAR(255),
-                    title VARCHAR(255),
-                    description VARCHAR(2048),
-                    images_json VARCHAR(3000),
-                    image_posted VARCHAR(255),
-                    image_posted_local VARCHAR(255),
+                    caption VARCHAR(3000),
+                    /* json encoded list of image ids, the first is the collage */
+                    image_ids VARCHAR(3000),
                     FOREIGN KEY (channel) REFERENCES channels(username)
-);
-
-/* sessions store where someone is within the conversation, and stage is 0 based */
-CREATE TABLE sessions (user_id VARCHAR(255) PRIMARY KEY,
-                       chat_type VARCHAR(255) DEFAULT 'customer',  # customer or admin
-                       channel VARCHAR(255),
-                       stage VARCHAR(255),
-                       FOREIGN KEY (channel) REFERENCES channels(username)
 );
