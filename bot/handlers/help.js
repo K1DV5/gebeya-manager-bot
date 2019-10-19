59cedcbab27daa2c99cad91300996326d6e19d37
Kidus Adugna <kidusadugna@gmail.com>
2019-10-19 12:51:20 +0300

    add help for permissions

--- bot/handlers/help.js
+++ bot/handlers/help.js
@@ -7,7 +7,20 @@ let helpItems = {
 
 let helpDetails = {
     post: 'Command /post\n\nThis command lets you make a new post. It asks the title of the post, the description and the price of the item. Then it produces a caption from a template (which is customizable through the /settings => "Caption template"). It also asks you photos of the item and produces a collage so that the channel will not be cluttered with many photos. If you have set a logo for the channel, it watermarks the photos so that your logo appears on them (including the collage). The watermark logo can be set in /settings => "Logo".',
-    settings: 'Command /settings\n\nThis command lets you customize some aspects of your channels. It has five components:\n\n<b>\u2022 Logo</b>: Here, you can set the logo of the channel that will be used to watermark the photos posted on it.\n<b>\u2022 Caption template</b>: You can set the template used to make the caption of the collage of the new item. You can include anything as long as you include the necessary components in any order.\n<b>\u2022 Contact text</b>: This is the text added on the caption of the item sent to the customer when they select "Buy" on an item.\n<b>\u2022 Sold template</b>: You can set how you want the caption to change when it is marked sold. It can contain anything as long as it contains the caption placeholder.\n<b>\u2022 Description bullet</b>: This setting lets you change the character(s) that replace the "." (dot) in the line beginnings of the description. This means you can list e.g. features of the item indicating the list items with . and it will replace the dot with whatever you define in this setting. The default is the bullet character (like in this text.)',
+    settings: 'Command /settings\n\nThis command lets you customize some aspects of your channels. It has five components:\n\n'
+        + Object.entries({
+            'Logo': 'Here, you can set the logo of the channel that will be used to watermark the photos posted on it.',
+
+            'Caption template': 'You can set the template used to make the caption of the collage of the new item. You can include anything as long as you include the necessary components in any order.',
+
+            'Contact text': 'This is the text added on the caption of the item sent to the customer when they select "Buy" on an item.',
+
+            'Sold template': 'You can set how you want the caption to change when it is marked sold. It can contain anything as long as it contains the caption placeholder.',
+
+            'Description bullet': 'This setting lets you change the character(s) that replace the "." (dot) in the line beginnings of the description. This means you can list e.g. features of the item indicating the list items with . and it will replace the dot with whatever you define in this setting. The default is the bullet character (like in this text.)',
+
+            'Update permissions': 'This setting retrieves admins\' permissions from Telegram so that the permissions can be used with the bot. Two permissions are retrieved: Post messages and Change channel info. The Post messages is used to decide if the person is allowed to post an item using the bot, and the Change channel info is used to decide if they are allowed to change channel settings (including this one). However, the person who is registered as the admin of the channel here will not be affected.'
+        }).map(([key, val]) => '<b>' + key + '</b>: ' + val).join('\n\n'),
     license: 'Command /license\n\nThis command shows the license information of your registered channels. You can see when it will expire (or if it has.) Licenses are shown per channel.\n\nWhen your channel\'s license expires, you will not be able to post new items, change settings, edit or repost existing items. However, already posted items will continue to be functional. You will get notifications when someone selects your item, you can mark an item sold (But not undo it).',
 }
 
