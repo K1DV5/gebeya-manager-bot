fdb494ed8a8298a7c355ac8bd0c0af12e20a4d85
Kidus Adugna <kidusadugna@gmail.com>
2019-10-20 12:33:08 +0300

    fix bugs on /cancel

--- bot/handlers/cancel.js
+++ bot/handlers/cancel.js
@@ -2,8 +2,8 @@
 async function handleCancel(ctx) {
     let username = ctx.from.username
     let convo = await ctx.people.getConvo(username)
-    if (currently) {
-        let about = currently.split('.', 1)[0]
+    if (convo) {
+        let about = convo.split('.', 1)[0]
         ctx.people.clearDraft(username)
         ctx.reply(about[0].toUpperCase() + about.slice(1) + ' cancelled.')
     } else {
