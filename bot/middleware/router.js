d29ada27a80934b6ea8af049fdff7747375e61e0
Kidus Adugna <kidusadugna@gmail.com>
2019-10-21 16:46:13 +0300

    router: await every async

--- bot/middleware/router.js
+++ bot/middleware/router.js
@@ -72,22 +72,22 @@ async function router(ctx) {
                 ctx.state.payload = text.slice(text.indexOf(' ') + 1)
                 if (command === '/start') {
                     if (text.split(' ').length > 1) {
-                        start.handleStart(ctx)
+                        await start.handleStart(ctx)
                     } else {
-                        start.handleWelcomeStart(ctx)
+                        await start.handleWelcomeStart(ctx)
                     }
                     return
                 } else if (command === '/post') {
-                    post.handlePost(ctx)
+                    await post.handlePost(ctx)
                     return
                 } else if (command === '/settings') {
-                    settings.handleSettings(ctx)
+                    await settings.handleSettings(ctx)
                     return
                 } else if (command === '/help') {
                     help.handleHelp(ctx)
                     return
                 } else if (command === '/license') {
-                    license.handleLicense(ctx)
+                    await license.handleLicense(ctx)
                     return
                 } else if (command === '/cancel') {
                     await cancel.handleCancel(ctx)
@@ -136,65 +136,65 @@ async function router(ctx) {
             }
         } else if (convo === 'post.photo') {
             if (updateSubTypes.includes('photo')) {
-                post.handlePhotoStagePhotos(ctx)
+                await post.handlePhotoStagePhotos(ctx)
                 return
             } else if (updateSubTypes.includes('text') && ctx.message.text === '/end') {
-                post.handlePhotoStageEnd(ctx)
+                await post.handlePhotoStageEnd(ctx)
             } else {
                 ctx.reply('Please send some photos for the post and finally send /end, or maybe look at the /help.')
             }
         } else if (convo === 'edit.title') {
             if (updateSubTypes.includes('text')) {
-                post.handleEditTitle(ctx)
+                await post.handleEditTitle(ctx)
                 return
             } else {
                 ctx.reply('Please send a text for the title, or maybe you need /help.')
             }
         } else if (convo === 'edit.description') {
             if (updateSubTypes.includes('text')) {
-                post.handleEditDescription(ctx)
+                await post.handleEditDescription(ctx)
                 return
             } else {
                 ctx.reply('Please send a text for the description, or maybe you need /help.')
             }
         } else if (convo === 'edit.price') {
             if (updateSubTypes.includes('text')) {
-                post.handleEditPrice(ctx)
+                await post.handleEditPrice(ctx)
                 return
             } else {
                 ctx.reply('Please send a text for the price, or maybe you need /help.')
             }
         } else if (convo === 'settings.logo.document') {
             if (updateSubTypes.includes('document')) {
-                settings.handleSettingLogoDoc(ctx)
+                await settings.handleSettingLogoDoc(ctx)
             } else {
                 ctx.reply('Please send an image FILE for the logo')
             }
             return
         } else if (convo === 'settings.caption_template.text') {
             if (updateSubTypes.includes('text')) {
-                settings.handleSettingTextCaptionTempl(ctx)
+                await settings.handleSettingTextCaptionTempl(ctx)
             } else {
                 ctx.reply('Please send a text for the caption template, or maybe you need /help.')
             }
             return
         } else if (convo === 'settings.sold_template.text') {
             if (updateSubTypes.includes('text')) {
-                settings.handleSettingTextSoldTempl(ctx)
+                await settings.handleSettingTextSoldTempl(ctx)
             } else {
                 ctx.reply('Please send a text for the sold template, or maybe you need /help.')
             }
             return
         } else if (convo === 'settings.contact_text.text') {
             if (updateSubTypes.includes('text')) {
-                settings.handleSettingTextContactText(ctx)
+                await settings.handleSettingTextContactText(ctx)
             } else {
                 ctx.reply('Please send a text for the contact text, or maybe you need /help.')
             }
             return
         } else if (convo === 'settings.description_bullet.text') {
             if (updateSubTypes.includes('text')) {
-                settings.handleSettingTextDescBullet(ctx)
+                await settings.handleSettingTextDescBullet(ctx)
             } else {
                 ctx.reply('Please send a text for the description bullet, or maybe you need /help.')
             }
